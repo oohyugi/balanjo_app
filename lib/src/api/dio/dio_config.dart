88a0ab3763448acbd699d1646699686d4731d720
Yogi Putra <mail.yogiputra@gmail.com>
2024-12-18 14:19:23 +0700

    Update dio_config.dart

--- lib/src/api/dio/dio_config.dart
+++ lib/src/api/dio/dio_config.dart
@@ -36,7 +36,7 @@ extension DioExt on Dio {
 
 Dio geocodingInterceptors({required Dio dio}) {
   return dio
-    ..interceptors.add(GeocodingInterceptor("AIzaSyDwaDc-GF8gS5cDPFFUjSOJVMpE3Y9EVBI"))
+    ..interceptors.add(GeocodingInterceptor("xxxxxx"))
     ..interceptors.add(CacheInterceptor())
     ..transformer = MyTransformer();
 }
@@ -53,9 +53,9 @@ class CustomInterceptors extends Interceptor {
     if(isRequiredAuth) {
       options.headers = {
       "apikey":
-          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZoY3R5ZmR1bmpxZXlhYnNqcWFsIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY5MDQzMDc5MiwiZXhwIjoyMDA2MDA2NzkyfQ.LDYanw9nlI1bLXnTFIkezmH65dDiiG1kZTueqmZ1vw4",
+          "xxx",
       "Authorization":
-          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZoY3R5ZmR1bmpxZXlhYnNqcWFsIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY5MDQzMDc5MiwiZXhwIjoyMDA2MDA2NzkyfQ.LDYanw9nlI1bLXnTFIkezmH65dDiiG1kZTueqmZ1vw4",
+          "Bearer xxx",
     };
 
     }
